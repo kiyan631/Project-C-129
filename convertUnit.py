7c0f6482077c5f7eb15ed2f5f731174bc4128589
kiyan631 <80814660+kiyan631@users.noreply.github.com>
2023-06-29 15:50:12 +0530

    Add files via upload

---
+++ convertUnit.py
@@ -0,0 +1,26 @@
+import csv 
+import pandas as pd
+
+data = pd.read_csv("star2_dwarf.csv")
+data = data.dropna()
+
+
+data["Radius"] = 0.102763*  data["Radius"]
+
+data['Mass']=data['Mass'].apply(lambda x: x.replace('$', '').replace(',', '')).astype('float')
+
+data['Mass'] = 0.000954588* data['Mass']
+
+data.to_csv("converted.csv")
+
+
+
+
+
+
+
+
+
+
+
+
